215f12068d91490e7211480b963b2593c9494f0e
tuannlse171583 <149662516+tuannlse171583@users.noreply.github.com>
2025-02-26 21:47:01 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,9 @@
+import torch
 import pytorch_lightning as pl
-
 from configs import get_config
 from models import construct_model
 from data import construct_data_module
 from utils import init_logger, construct_callbacks
-
 
 if __name__ == "__main__":
     config = get_config()
@@ -13,6 +12,16 @@ if __name__ == "__main__":
     cem_model = construct_model(config, data_module.imbalance_weight)
     logger = init_logger(config)
     callbacks = construct_callbacks(config)
+
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        dirpath=config["ckpt_save_dir"],
+        filename="last",  # Lưu với tên 'last.ckpt'
+        save_top_k=1,
+        monitor=config["ckpt_save_monitor"],
+        mode=config["ckpt_save_mode"],
+        save_weights_only=True,
+    )
+    callbacks.append(checkpoint_callback)
 
     trainer = pl.Trainer(
         accelerator="gpu",
@@ -26,3 +35,6 @@ if __name__ == "__main__":
 
     trainer.fit(cem_model, datamodule=data_module)
     trainer.test(cem_model, datamodule=data_module)
+
+    trainer.save_checkpoint(f"{config['ckpt_save_dir']}/last.ckpt")
+    print("Mô hình đã được lưu tại", f"{config['ckpt_save_dir']}/last.ckpt")
